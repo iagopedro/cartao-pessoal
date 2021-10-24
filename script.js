75d31c6c41d64803ba41de087323cd394edfbbec
iagopedro <iagopedroltd19@gmail.com>
2021-10-24 17:06:16 -0300

    first commit  :tada:

---
+++ script.js
@@ -0,0 +1,37 @@
+const LinksSocialMedia = {
+  github: 'iagopedro',
+  youtube: '/channel/UCtV3ciGwoQ9mAraWTMvBDIQ',
+  instagram: 'iagopedro.js',
+  facebook: 'iago.pedro.7',
+  twitter: '',
+}
+
+
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class');
+    
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGithubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      githubUserName.textContent = data.name
+      githubUserBio.textContent = data.bio
+      githubUserProfile.textContent = data.login
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+    })
+
+  alert(url)
+}
+
+getGithubProfileInfos()
